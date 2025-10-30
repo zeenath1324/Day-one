4e880a39124f4746c46492be00a58848d8c44ec5
Zeenath Nisha Shajahan <zeenathnisha2411@gmail.com>
2025-10-30 18:31:27 +0530

    Add files via upload

---
+++ SumOfNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+
+        sc.close();
+    }
+}
